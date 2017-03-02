c86647fa30295077cf565d4decc5900a8cc4f815
Steven Feldman <stevenfeldman@Stevens-MBP.fios-router.home>
2017-03-01 21:32:08 -0500

    Checkpoint 5: create and implement controllers

---
+++ dist/scripts/fixtures.js
@@ -0,0 +1,45 @@
+var albumPicasso = {
+        title: 'The Colors',
+        artist: 'Pablo Picasso',
+        label: 'Cubism',
+        year: '1881',
+        albumArtUrl: 'assets/images/album_covers/01.png',
+        songs: [
+         { title: 'Blue', duration: 161.71, audioUrl: 'assets/music/blue' },
+         { title: 'Green', duration: 103.96, audioUrl: 'assets/music/green' },
+         { title: 'Red', duration: 268.45, audioUrl: 'assets/music/red' },
+         { title: 'Pink', duration: 153.14, audioUrl: 'assets/music/pink' },
+         { title: 'Magenta', duration: 374.22, audioUrl: 'assets/music/magenta' }
+        ]
+    };
+ 
+var albumMarconi = {
+        title: 'The Telephone',
+        artist: 'Guglielmo Marconi',
+        label: 'EM',
+        year: '1909',
+        albumArtUrl: 'assets/images/album_covers/20.png',
+        songs: [
+            { title: 'Hello, Operator?', duration: '1:01' },
+            { title: 'Ring, ring, ring', duration: '5:01' },
+            { title: 'Fits in your pocket', duration: '3:21'},
+            { title: 'Can you hear me now?', duration: '3:14' },
+            { title: 'Wrong phone number', duration: '2:15'}
+        ]
+    };
+
+var albumMacklemore = {
+    title: "The Heist",
+    artist: "Macklemore and Ryan Lewis",
+    label: "none",
+    year: "2012",
+    albumArtUrl: "assets/images/album_covers/Macklemore.png",
+    songs: [
+        { title: "Ten Thousand Hours", duration: "4:10"},
+        { title: "Can't Hold Us ft Ray Dalton", duration: "4:18"},
+        { title: "Thrift Shop ft Wanz", duration: "3:56"},
+        { title: "Thin line ft Buffalo Madonna", duration: "4:16"},
+        { title: "Same Love ft Mary Lambert", duration: "5:20"},
+        { title: "Make the Money", duration: "3:45"}
+    ]
+};
